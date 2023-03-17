f6e3aae44ff8636037e3aabe4205948461ef08b0
MrDj200 <david@klatt-luedenscheid.de>
2023-03-17 05:00:28 +0100

    Add project files.

---
+++ BotConsole/Program.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Gateway.Commands;
+using Remora.Discord.Gateway;
+using Remora.Discord.Gateway.Extensions;
+using Remora.Discord.Gateway.Results;
+using Remora.Results;
+
+namespace BotConsole
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                MainAsync(args).Wait();
+            }
+            catch (Exception e)
+            {
+                if (e is TaskCanceledException)
+                {
+                    Console.WriteLine("Task canceled");
+                    return;
+                }
+                if (e is AggregateException && e.Message.Contains("canceled"))
+                {
+                    Console.WriteLine("Aggregate contains canceled");
+                    return;
+                }
+                throw;
+            }
+            finally { Console.WriteLine("I'm outta here!"); }
+
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Starting bot!");
+
+            var cancellationSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                Console.WriteLine("Ending program...");
+                eventArgs.Cancel = true;
+                cancellationSource.Cancel();
+            };
+
+            var botToken = args[0];
+
+            var services = new ServiceCollection()
+                .AddDiscordGateway(_ => botToken)
+                .AddResponder<PingPongResponder>()
+                .AddResponder<TwitterResponder>()
+                .Configure<DiscordGatewayClientOptions>(g => g.Intents |= GatewayIntents.MessageContents | GatewayIntents.GuildPresences)
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddConsole();
+                })
+                .BuildServiceProvider();
+
+            var gatewayClient = services.GetRequiredService<DiscordGatewayClient>();
+
+            var runResult = await gatewayClient.RunAsync(cancellationSource.Token);
+
+            var log = services.GetRequiredService<ILogger<Program>>();
+
+            if (!runResult.IsSuccess)
+            {
+                switch (runResult.Error)
+                {
+                    case ExceptionError exe:
+                        {
+                            log.LogError
+                            (
+                                exe.Exception,
+                                "Exception during gateway connection: {ExceptionMessage}",
+                                exe.Message
+                            );
+
+                            break;
+                        }
+                    case GatewayWebSocketError:
+                    case GatewayDiscordError:
+                        {
+                            log.LogError("Gateway error: {Message}", runResult.Error.Message);
+                            break;
+                        }
+                    default:
+                        {
+                            log.LogError("Unknown error: {Message}", runResult.Error.Message);
+                            break;
+                        }
+                }
+            }
+
+            await Task.Delay(-1, cancellationSource.Token);
+        }
+    }
+
+}
---
+++ BotConsole/TwitterResponder.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+using System.Text.RegularExpressions;
+
+namespace BotConsole
+{
+    public class TwitterResponder : IResponder<IMessageCreate>
+    {
+        private readonly IDiscordRestChannelAPI _channelAPI;
+        private readonly ILogger<TwitterResponder> _logger;
+
+        public TwitterResponder(IDiscordRestChannelAPI channelAPI, ILogger<TwitterResponder> logger)
+        {
+            _channelAPI = channelAPI;
+            _logger = logger;
+        }
+
+        public async Task<Result> RespondAsync(IMessageCreate gatewayEvent, CancellationToken ct = default)
+        {
+            if (gatewayEvent.Author.IsBot.HasValue && gatewayEvent.Author.IsBot.Value || gatewayEvent.WebhookID.HasValue)
+            {
+                return Result.FromSuccess();
+            }
+
+            var match = Regex.Match(gatewayEvent.Content, @"https?:\/\/twitter\.com\/\w+\/status\/\d+(\/photos\/\d)?");
+
+            if (!match.Success)
+            {
+                return Result.FromSuccess();
+            }
+
+            await Task.Delay(1000, ct); // Wait a second for slow twitter to get an embed
+
+            await _channelAPI.EditMessageAsync(gatewayEvent.ChannelID, gatewayEvent.ID, embeds: null, flags: MessageFlags.SuppressEmbeds); // Remove the original embed
+
+            _logger.LogInformation($"Converting twitter link {match.Value}");
+
+            return (Result)await _channelAPI.CreateMessageAsync
+            (
+                gatewayEvent.ChannelID,
+                content: $"It's dangerous to go alone! Here, take this (better) embed: {match.Value.Replace("twitter.com", "fxtwitter.com")}",
+                ct: ct
+            );
+        }
+    }
+
+}
