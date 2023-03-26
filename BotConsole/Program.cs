5960fdef1ffb5c60d83a69b20790f4e67a3af878
MrDj200 <david@klatt-luedenscheid.de>
2023-03-26 09:30:13 +0200

    Refactor to use Generic Host

--- BotConsole/Program.cs
+++ BotConsole/Program.cs
@@ -1,22 +1,18 @@
 ﻿using BotConsole.Commands;
+using BotConsole.Workers;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Remora.Commands.Extensions;
 using Remora.Discord.API.Abstractions.Gateway.Commands;
 using Remora.Discord.Commands.Extensions;
-using Remora.Discord.Commands.Services;
 using Remora.Discord.Gateway;
 using Remora.Discord.Gateway.Extensions;
-using Remora.Discord.Gateway.Results;
-using Remora.Results;
 
 namespace BotConsole
 {
     internal class Program
     {
-
-        private static string _botToken = "";
-
         static void Main(string[] args)
         {
 
@@ -25,125 +21,40 @@ namespace BotConsole
 #else
             Console.WriteLine("Starting in Release mode!");
 #endif
-            try
-            {
-                MainAsync(args).Wait();
-            }
-            catch (Exception e)
-            {
-                if (e is TaskCanceledException)
-                {
-                    Console.WriteLine("Task canceled");
-                    return;
-                }
-                if (e is AggregateException && e.Message.Contains("canceled"))
-                {
-                    Console.WriteLine("Aggregate contains canceled");
-                    return;
-                }
-                throw;
-            }
-            finally { Console.WriteLine("I'm outta here!"); }
 
-        }
-
-        static async Task MainAsync(string[] args)
-        {
-            Console.WriteLine("Starting bot!");
-
-            var cancellationSource = new CancellationTokenSource();
-            Console.CancelKeyPress += (sender, eventArgs) =>
-            {
-                Console.WriteLine("Ending program...");
-                eventArgs.Cancel = true;
-                cancellationSource.Cancel();
-            };
-
-            _botToken = args[0];
+            var _botToken = args[0];
             if (string.IsNullOrEmpty(_botToken))
             {
                 Console.WriteLine("NO BOT TOKEN GIVEN IN PARAMETER!");
                 return;
             }
 
-            var servicesRaw = ConfigureServices(new ServiceCollection());
-
-            //var servicesRaw = new ServiceCollection()
-
-
-            servicesRaw.AddDiscordCommands(true).AddCommandTree()
-                .WithCommandGroup<HttpCatCommands>()
-                .WithCommandGroup<MessageStats>()
-                .WithCommandGroup<VRCXMetadataResolve>();
-
-
-            var services = servicesRaw.BuildServiceProvider();
-            var log = services.GetRequiredService<ILogger<Program>>();
-
-            #region SlashCommandStuff?
-            var slashService = services.GetRequiredService<SlashService>();
-            var updateSlash = await slashService.UpdateSlashCommandsAsync(ct: cancellationSource.Token);
-            if (!updateSlash.IsSuccess)
-            {
-                log.LogWarning("Failed to update slash commands: {Reason}", updateSlash.Error.Message);
-            }
-            #endregion
-
-            var gatewayClient = services.GetRequiredService<DiscordGatewayClient>();
-            var runResult = await gatewayClient.RunAsync(cancellationSource.Token);
-
-            if (!runResult.IsSuccess)
-            {
-                switch (runResult.Error)
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
                 {
-                    case ExceptionError exe:
+                    services
+                        .AddDiscordGateway(_ => _botToken)
+                        .AddResponder<TwitterResponder>()
+                        .AddResponder<RockAndStoneResponder>()
+                        .AddSingleton<DRGMessageProvider>()
+                        .Configure<DiscordGatewayClientOptions>(g => g.Intents |= GatewayIntents.MessageContents | GatewayIntents.GuildPresences)
+                        .AddSingleton<HttpClient>()
+                        .AddLogging(loggingBuilder =>
                         {
-                            log.LogError
-                            (
-                                exe.Exception,
-                                "Exception during gateway connection: {ExceptionMessage}",
-                                exe.Message
-                            );
+                            loggingBuilder.AddConsole();
+                        });
 
-                            break;
-                        }
-                    case GatewayWebSocketError:
-                    case GatewayDiscordError:
-                        {
-                            log.LogError("Gateway error: {Message}", runResult.Error.Message);
-                            break;
-                        }
-                    default:
-                        {
-                            log.LogError("Unknown error: {Message}", runResult.Error.Message);
-                            break;
-                        }
-                }
-            }
+                    services.AddDiscordCommands(true).AddCommandTree()
+                        .WithCommandGroup<HttpCatCommands>()
+                        .WithCommandGroup<MessageStats>()
+                        .WithCommandGroup<VRCXMetadataResolve>();
 
-            await Task.Delay(-1, cancellationSource.Token);
-        }
+                    services
+                        .AddHostedService<RemoraBotWorker>();
 
-        private static IServiceCollection ConfigureServices(IServiceCollection services)
-        {
+                }).Build();
 
-            services // Discord specific stuff
-                .AddDiscordGateway(_ => _botToken)
-                .AddResponder<TwitterResponder>()
-                .AddResponder<RockAndStoneResponder>()
-                .AddSingleton<DRGMessageProvider>()
-                .Configure<DiscordGatewayClientOptions>(g => g.Intents |= GatewayIntents.MessageContents | GatewayIntents.GuildPresences);
-
-            services // Misc stuff
-                .AddSingleton<HttpClient>();
-
-            services // Logging
-                .AddLogging(loggingBuilder =>
-                {
-                    loggingBuilder.AddConsole();
-                });
-
-            return services;
+            host.Run();
         }
     }
 
---
+++ BotConsole/Workers/RemoraBotWorker.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Remora.Discord.Commands.Services;
+using Remora.Discord.Gateway;
+using Remora.Discord.Gateway.Results;
+using Remora.Results;
+
+namespace BotConsole.Workers
+{
+    internal class RemoraBotWorker : BackgroundService
+    {
+        private readonly ILogger<RemoraBotWorker> _logger;
+        private readonly SlashService _slashService;
+        private readonly DiscordGatewayClient _gatewayClient;
+
+        public RemoraBotWorker(ILogger<RemoraBotWorker> logger, SlashService slashService, DiscordGatewayClient gatewayClient)
+        {
+            _logger = logger;
+            _slashService = slashService;
+            _gatewayClient = gatewayClient;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("TRYING TO GRACEFULLY SHUTDOWN!");
+            _gatewayClient.Dispose();
+            return Task.CompletedTask;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Starting bot!");
+
+            var updateSlash = await _slashService.UpdateSlashCommandsAsync(ct: stoppingToken);
+            if (!updateSlash.IsSuccess)
+            {
+                _logger.LogWarning("Failed to update slash commands: {Reason}", updateSlash.Error.Message);
+            }
+
+            var runResult = await _gatewayClient.RunAsync(stoppingToken);
+
+            if (!runResult.IsSuccess)
+            {
+                switch (runResult.Error)
+                {
+                    case ExceptionError exe:
+                        {
+                            _logger.LogError
+                            (
+                                exe.Exception,
+                                "Exception during gateway connection: {ExceptionMessage}",
+                                exe.Message
+                            );
+
+                            break;
+                        }
+                    case GatewayWebSocketError:
+                    case GatewayDiscordError:
+                        {
+                            _logger.LogError("Gateway error: {Message}", runResult.Error.Message);
+                            break;
+                        }
+                    default:
+                        {
+                            _logger.LogError("Unknown error: {Message}", runResult.Error.Message);
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
