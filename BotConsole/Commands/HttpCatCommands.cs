ccfe65880a3dc88a0f1a03510718d54af8ac7d28
MrDj200 <david@klatt-luedenscheid.de>
2023-03-26 17:13:23 +0200

    remove unneeded commands in release again

--- BotConsole/Commands/HttpCatCommands.cs
+++ BotConsole/Commands/HttpCatCommands.cs
@@ -81,9 +81,9 @@ namespace BotConsole.Commands
 
 
             return (Result)await _feedbackService.SendContextualMessageAsync(new FeedbackMessage(ugh, _feedbackService.Theme.FaultOrDanger), options: opt);
-        }        
+        }
 
-
+#if DEBUG
         [Command("cat")]
         [Description("Posts a cat image that represents the given error code.")]
         public async Task<IResult> PostHttpCatAsync([Description("The HTTP code.")] int httpCode)
@@ -138,5 +138,6 @@ namespace BotConsole.Commands
         {
             return ((value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget)) + fromTarget;
         }
+#endif
     }
 }
